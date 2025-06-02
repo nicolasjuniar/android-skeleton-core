0ccdafbb5ac7ab3c5875c57dcba62f273dd0758a
Nicolas Juniar Kurniawan <nicolas@investree.id>
2025-06-02 15:50:48 +0700

    [nico] remove unused class

--- library/src/main/kotlin/juniar/nicolas/androidskeletoncore/di/NetworkModule.kt
+++ library/src/main/kotlin/juniar/nicolas/androidskeletoncore/di/NetworkModule.kt
@@ -7,7 +7,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import juniar.nicolas.androidskeletoncore.data.BaseApiService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -37,11 +36,6 @@ object NetworkModule {
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): BaseApiService =
-        retrofit.create(BaseApiService::class.java)
 }
 
 @Qualifier
